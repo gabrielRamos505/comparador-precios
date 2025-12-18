cdb92dc6b972c214222ea5cd1ea57324d05912d3
gabrielRamos505 <rustandree529@gmail.com>
2025-12-18 10:14:30 -0500

    fix: restaurar instalación de Chrome en script de construcción de Render

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
+# Instalar dependencias
+npm install
+
+# Instalar navegador Chrome para Puppeteer
+# Esto lo descarga en /opt/render/.cache/puppeteer por defecto en Render
+npx puppeteer browsers install chrome
